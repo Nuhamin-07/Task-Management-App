98f987ebd4b0bec062fd7dd63d6223d784128cfc
Nuhamin <ngulilat@myfuse1.education>
2026-05-12 08:45:17 -0400

    all CRUD requests handled from the server side

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -13,3 +13,27 @@ export async function postTask(req, res) {
 
   res.status(201).json({ message: "Task created successfully" });
 }
+
+export async function getTasks(req, res) {
+  const tasks = await getAllTasks();
+  res.json(tasks);
+}
+
+export async function getIndividualTask(req, res) {
+  const { id } = req.params;
+  const task = await getTaskById(id);
+  res.json(task);
+}
+
+export async function updateIndividualTask(req, res) {
+  const { id } = req.params;
+  const { name, description, priority, status, completed_at } = req.body;
+  await updateTask(id, name, description, priority, status, completed_at);
+  res.json({ message: "Task updated successfully" });
+}
+
+export async function deleteIndividualTask(req, res) {
+  const { id } = req.params;
+  await deleteTask(id);
+  res.json({ message: "Task deleted successfully" });
+}
--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
-import { postTask } from "../controllers/taskController.js";
+import {
+  postTask,
+  getTasks,
+  getIndividualTask,
+  updateIndividualTask,
+  deleteIndividualTask,
+} from "../controllers/taskController.js";
 
 export const taskRouter = Router();
 
 taskRouter.post("/tasks", postTask);
+taskRouter.get("/tasks", getTasks);
+taskRouter.get("/tasks/:id", getIndividualTask);
+taskRouter.put("/tasks/:id", updateIndividualTask);
+taskRouter.delete("/tasks/:id", deleteIndividualTask);
