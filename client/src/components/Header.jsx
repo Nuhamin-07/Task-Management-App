880a52e4f11cbe9d38209759691a0e23984e656a
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-05-12 14:39:10 -0400

    css added for the navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <div>
+    <div id="navbar">
       <Button>Click Me</Button>
     </div>
   );
