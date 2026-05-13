6c13eeb906169e55983db3a9de4f5235d39eebe6
Nuhamin <ngulilat@myfuse1.education>
2026-05-13 13:36:50 -0400

    fetching data from the frontend handled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import router from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <TooltipProvider>
+      <RouterProvider router={router} />
+    </TooltipProvider>
+  );
 }
 
 export default App;
