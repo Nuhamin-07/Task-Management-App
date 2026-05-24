2b40db828888d04ca69ebb422dc66e159a8374de
Nuhamin <nuhamin.gulilat.7@gmail.com>
2026-05-24 16:19:41 -0400

    authentication issue fixed

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -7,33 +7,42 @@ import {
 } from "../sql/handleData.js";
 
 export async function postTask(req, res) {
-  const { name, description, priority, status, completed_at } = req.body;
+  const { name, description, priority, status, completed_at, user_id } =
+    req.body;
 
-  await insertTask(name, description, priority, status, completed_at);
+  await insertTask(name, description, priority, status, completed_at, user_id);
 
   res.status(201).json({ message: "Task created successfully" });
 }
 
 export async function getTasks(req, res) {
-  const tasks = await getAllTasks();
+  const tasks = await getAllTasks(req.session.userId);
   res.json(tasks);
 }
 
 export async function getIndividualTask(req, res) {
   const { id } = req.params;
-  const task = await getTaskById(id);
+  const task = await getTaskById(id, req.session.userId);
   res.json(task);
 }
 
 export async function updateIndividualTask(req, res) {
   const { id } = req.params;
   const { name, description, priority, status, completed_at } = req.body;
-  await updateTask(id, name, description, priority, status, completed_at);
+  await updateTask(
+    id,
+    name,
+    description,
+    priority,
+    status,
+    completed_at,
+    req.session.userId,
+  );
   res.json({ message: "Task updated successfully" });
 }
 
 export async function deleteIndividualTask(req, res) {
   const { id } = req.params;
-  await deleteTask(id);
+  await deleteTask(id, req.session.userId);
   res.json({ message: "Task deleted successfully" });
 }
